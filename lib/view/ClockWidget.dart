0abb325910fee8c7517e320aa8d1bec777822347
tongochona <ngochoan15032003@gmail.com>
2024-11-27 18:20:50 +0700

    upload clock widget

--- lib/view/ClockWidget.dart
+++ lib/view/ClockWidget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ClockWidget extends StatefulWidget {
@@ -8,36 +10,73 @@ class ClockWidget extends StatefulWidget {
 }
 
 class _ClockWidgetState extends State<ClockWidget> {
+  DateTime datetime = DateTime.now();
 
-  String datetime = DateTime.now().toString();
   @override
   Widget build(BuildContext context) {
+    Color clockColor;
+    Random random = Random();
+    int r = random.nextInt(255);
+    int g = random.nextInt(255);
+    int b = random.nextInt(255);
+    clockColor = Color.fromARGB(255, r, g, b);
+    String date = "${datetime.day}/${datetime.month}/${datetime.year}";
+    String time =
+        "${datetime.hour}:${datetime.minute < 10 ? "0${datetime.minute}" : datetime.minute}:${datetime.second}";
     return Scaffold(
-      backgroundColor: Colors.grey[600],
-      floatingActionButton: FloatingActionButton.extended(onPressed: (){
-        setState(() {
-          datetime=DateTime.now().toString();
-        });
-      },
-      label:const Text("Cập nhật"),
-      backgroundColor: Colors.white,
-      splashColor: Colors.blue,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-           const Icon(
-            Icons.access_time_filled_sharp,
-            size: 200,
-            color: Colors.white,
-            ),
-            Text(datetime,
-            style:const TextStyle(
-              color: Colors.white,
-              fontSize: 50
-            ),)
-        ],
-      )
-    );
+        backgroundColor: Colors.grey[600],
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {
+              datetime = DateTime.now();
+              r = random.nextInt(255);
+              g = random.nextInt(255);
+              b = random.nextInt(255);
+              clockColor = Color.fromARGB(255, r, g, b);
+            });
+          },
+          label: const Text("Cập nhật"),
+          backgroundColor: Colors.white,
+          splashColor: Colors.blue,
+        ),
+        //floatingActionButtonLocation: FloatingActionButtonLocation.,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/anh.jpg"), fit: BoxFit.fitHeight),
+          ),
+          alignment: Alignment.topCenter,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.access_time_filled_sharp,
+                size: 200,
+                color: clockColor,
+              ),
+              Container(
+                //transform: Matrix4.rotationZ(0.1),
+                decoration: const BoxDecoration(
+                    gradient:
+                        LinearGradient(colors: [Colors.pink, Colors.blue])),
+                child: Text(
+                  date,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+              Text(
+                time,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  backgroundColor: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
