ce29efc700626611b6f0f31de78fd8c5628af5fb
tongochona <ngochoan15032003@gmail.com>
2024-11-27 18:36:29 +0700

    create Timer Widget

---
+++ lib/view/TimerWidget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TimerWidget extends StatefulWidget {
+  const TimerWidget({super.key});
+
+  @override
+  State<TimerWidget> createState() => _TimerWidgetState();
+}
+
+class _TimerWidgetState extends State<TimerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
